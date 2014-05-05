806d78d4115ab1afd275bcd6e150353bd0d844c9
amephraim <ame378@nyu.edu>
2014-05-05 19:43:41 -0400

    mergeing

--- code/relation_extracter.py
+++ code/relation_extracter.py
@@ -173,7 +173,7 @@ if __name__=="__main__":
 	b.readMaleNames()
 	b.readFemaleNames()
 	
-	rawtext = open("gen5").read() 
+	rawtext = open("gen5.txt").read() 
 	sentences = rawtext.replace(";",".")
 	clauses= sentences.split(".")
 	for sent in clauses:
@@ -193,7 +193,7 @@ if __name__=="__main__":
 	catches= [ x for x in b.catches if x not in seen and not seen_add(x)]
 	for c in catches:
 		print c
-	testing(catches,"gen5correct")
+	testing(catches,"gen5correct.txt")
 # 	#with open ("results2.txt","w") as r:
 # 	for c in catches:
 # 		print c
