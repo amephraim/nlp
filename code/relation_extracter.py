62aeb1c0c5c2cb7d5ae60a04fb14a5a46c04bbc5
amephraim <ame378@nyu.edu>
2014-05-05 15:40:18 -0400

    daughters stashed

--- code/relation_extracter.py
+++ code/relation_extracter.py
@@ -61,8 +61,9 @@ class extractBible:
 			self.write("%s is the mother of %s"%(mother,child))
 
 	def recursivePattern(self,sent,tagNames):
-
-		recpattern = re.compile(".* <Man ([A-Z][a-z]+)>.* the son of <Man ([A-Z][a-z]+)>.* (which|Which was the son of <Man ([A-Z][a-z]+)>.*)*.*")
+		print sent
+# 		recpattern = re.compile(".* <Man ([A-Z][a-z]+)>.* the son of <Man ([A-Z][a-z]+)>.* (which|Which was the son of <Man ([A-Z][a-z]+)>.*)*.*")
+		recpattern = re.compile(".* <Man ([A-Z][a-z]+)>.* the son of <Man ([A-Z][a-z]+)>.*( which|Which was the son of <Man ([A-Z][a-z]+)>)+")
 		if recpattern.match(sent):
 			j=1
 			for i in xrange(len(tagNames)-1):
@@ -73,7 +74,6 @@ class extractBible:
 		
 	def begatManyPattern(self,sent):
 		manypattern = re.compile("<Man ([A-Z][a-z]+)>.* begat <Man ([A-Z][a-z]+)>.* and <Man ([A-Z][a-z]+)>")
-
 		foundManyPattern = re.findall(manypattern, sent)
 		for match in foundManyPattern:
 			print "begatManyPattern: " 
@@ -85,6 +85,28 @@ class extractBible:
 # 		print text
 		self.catches.append(text)	
 
+	def espousePattern(self,sent):
+		espousepattern = re.compile("<Woman ([A-Z][a-z]+)> was espoused to <Man ([A-Z][a-z]+)>")
+		espousewife = re.findall(espousepattern,sent)
+		for match in espousewife:
+			self.write("%s is the wife of %s"%(match[0],match[1]))
+		husbandpattern = re.compile("<Man ([A-Z][a-z]+)> the husband of <Woman ([A-Z][a-z]+)>")
+		husbandwife = re.findall(husbandpattern,sent)
+		for match in husbandwife:
+			self.write("%s is the wife of %s"%(match[1],match[0]))
+	
+	def sonofPattern(self,sent):
+		sonpattern = re.compile("<Man ([A-Z][a-z]+)> the? son of <Man ([A-Z][a-z]+)>")
+		foundson = re.findall(sonpattern,sent)
+		for match in foundson:
+			self.write("%s is the father of %s"%(match[1],match[0]))
+	
+	def daughterofPattern(self,sent):
+		daughterpattern = re.compile("<Woman ([A-Z][a-z]+)> the? daughter of <Man ([A-Z][a-z]+)>")
+		founddaughter = re.findall(daughterpattern,sent)
+		for match in founddaughter:
+			self.write("%s is the father of %s"%(match[1],match[0]))
+		
 def cleanClause(sent):
 	nopattern = re.compile("[1-9][0-9]*:[1-9][0-9]")
 	words = sent.split()
@@ -101,41 +123,47 @@ if __name__=="__main__":
 	b = extractBible()
 	b.readMaleNames()
 	b.readFemaleNames()
-	rawtext = """3:23 And Jesus himself began to be about thirty years of age, being
-(as was supposed) the son of Joseph, which was the son of Heli, 3:24
-Which was the son of Matthat, which was the son of Levi, which was the
-son of Melchi, which was the son of Janna, which was the son of
-Joseph, 3:25 Which was the son of Mattathias, which was the son of
-Amos, which was the son of Naum, which was the son of Esli, which was
-the son of Nagge, 3:26 Which was the son of Maath, which was the son
-of Mattathias, which was the son of Semei, which was the son of
-Joseph, which was the son of Juda, 3:27 Which was the son of Joanna,
-which was the son of Rhesa, which was the son of Zorobabel, which was
-the son of Salathiel, which was the son of Neri, 3:28 Which was the
-son of Melchi, which was the son of Addi, which was the son of Cosam,
-which was the son of Elmodam, which was the son of Er, 3:29 Which was
-the son of Jose, which was the son of Eliezer, which was the son of
-Jorim, which was the son of Matthat, which was the son of Levi, 3:30
-Which was the son of Simeon, which was the son of Juda, which was the
-son of Joseph, which was the son of Jonan, which was the son of
-Eliakim, 3:31 Which was the son of Melea, which was the son of Menan,
-which was the son of Mattatha, which was the son of Nathan, which was
-the son of David, 3:32 Which was the son of Jesse, which was the son
-of Obed, which was the son of Booz, which was the son of Salmon, which
-was the son of Naasson, 3:33 Which was the son of Aminadab, which was
-the son of Aram, which was the son of Esrom, which was the son of
-Phares, which was the son of Juda, 3:34 Which was the son of Jacob,
-which was the son of Isaac, which was the son of Abraham, which was
-the son of Thara, which was the son of Nachor, 3:35 Which was the son
-of Saruch, which was the son of Ragau, which was the son of Phalec,
-which was the son of Heber, which was the son of Sala, 3:36 Which was
-the son of Cainan, which was the son of Arphaxad, which was the son of
-Sem, which was the son of Noe, which was the son of Lamech, 3:37 Which
-was the son of Mathusala, which was the son of Enoch, which was the
-son of Jared, which was the son of Maleleel, which was the son of
-Cainan, 3:38 Which was the son of Enos, which was the son of Seth,
-which was the son of Adam, which was the son of God."""
-# 	rawtext = open("trainer.txt").read() 
+# 	rawtext = """
+#  	When as his mother Joseph the wife of Joseph, before they came together.
+#  	she was found with child of the Holy Ghost.
+#  	Then Zerubbabel the son of Shealtiel, and Joshua the son of Jehozadak, the high priest, with all the remnant of the 
+# 	saying, Joseph, thou son of David, fear not to take unto thee Mary thy wife."""
+# 	And Jesus himself began to be about thirty years of age, being
+# (as was supposed) the son of Joseph, which was the son of Heli, 3:24
+# Which was the son of Matthat, which was the son of Levi, which was the
+# son of Melchi, which was the son of Janna, which was the son of
+# Joseph, 3:25 Which was the son of Mattathias, which was the son of
+# Amos, which was the son of Naum, which was the son of Esli, which was
+# the son of Nagge, 3:26 Which was the son of Maath, which was the son
+# of Mattathias, which was the son of Semei, which was the son of
+# Joseph, which was the son of Juda, 3:27 Which was the son of Joanna,
+# which was the son of Rhesa, which was the son of Zorobabel, which was
+# the son of Salathiel, which was the son of Neri, 3:28 Which was the
+# son of Melchi, which was the son of Addi, which was the son of Cosam,
+# which was the son of Elmodam, which was the son of Er, 3:29 Which was
+# the son of Jose, which was the son of Eliezer, which was the son of
+# Jorim, which was the son of Matthat, which was the son of Levi, 3:30
+# Which was the son of Simeon, which was the son of Juda, which was the
+# son of Joseph, which was the son of Jonan, which was the son of
+# Eliakim, 3:31 Which was the son of Melea, which was the son of Menan,
+# which was the son of Mattatha, which was the son of Nathan, which was
+# the son of David, 3:32 Which was the son of Jesse, which was the son
+# of Obed, which was the son of Booz, which was the son of Salmon, which
+# was the son of Naasson, 3:33 Which was the son of Aminadab, which was
+# the son of Aram, which was the son of Esrom, which was the son of
+# Phares, which was the son of Juda, 3:34 Which was the son of Jacob,
+# which was the son of Isaac, which was the son of Abraham, which was
+# the son of Thara, which was the son of Nachor, 3:35 Which was the son
+# of Saruch, which was the son of Ragau, which was the son of Phalec,
+# which was the son of Heber, which was the son of Sala, 3:36 Which was
+# the son of Cainan, which was the son of Arphaxad, which was the son of
+# Sem, which was the son of Noe, which was the son of Lamech, 3:37 Which
+# was the son of Mathusala, which was the son of Enoch, which was the
+# son of Jared, which was the son of Maleleel, which was the son of
+# Cainan, 3:38 Which was the son of Enos, which was the son of Seth,
+# which was the son of Adam, which was the son of God.
+# 	
+	rawtext = open("gen36").read() 
  	#rawtext= "40:001:002 Abraham begat Isaac; and Isaac begat Jacob; and Jacob begat Judas and Park of Ruth;"
 	sentences = rawtext.replace(";",".")
 	clauses= sentences.split(".")
@@ -146,6 +174,9 @@ which was the son of Adam, which was the son of God."""
 		b.begatManyPattern(sent)
 		b.begatPattern(sent)
 		b.recursivePattern(sent, tagNames)
+		b.espousePattern(sent)
+		b.sonofPattern(sent)
+		b.daughterofPattern(sent)
 	seen = set()
 	seen_add = seen.add
 	catches= [ x for x in b.catches if x not in seen and not seen_add(x)]
